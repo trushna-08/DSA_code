b24ce1d92c8d8bbaa18fae48cdd55e3cc19f84ca
Trushna Agrawal <163284668+trushna-08@users.noreply.github.com>
2026-01-09 15:42:51 +0530

    Add files via upload

---
+++ ArrayList_1.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+class ArrayList_1 {
+    public static void main(String[] args) {
+    
+        ArrayList<Integer> list = new ArrayList<Integer>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        System.out.println(list);
+        
+    }
+}
